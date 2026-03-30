9eee285e7f2cd8c5513d7d7e1397bbf346d0b6a4
sbinsalman <233876342+sbinsalm@users.noreply.github.com>
2026-03-29 20:47:41 -0700

    removed old g7 repo

--- defects-discovered/pmd/routes/GithubRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { cloneRepo } from "../controllers/GithubController.js" 
-
-const router = Router()
-
-router.post("/clone", cloneRepo)
-
-export default router
