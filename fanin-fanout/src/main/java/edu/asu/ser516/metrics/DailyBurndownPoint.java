2b61835394e0061430578bcb254386e8c68659e5
firefreedom3 <sander95@asu.edu>
2026-04-28 15:14:28 -0700

    Revert "Merge pull request #82 from DeepanshuSaxena14/task-149-daily-burndown"
    This reverts commit e6708f7ab7ff780fbed6db49746800c407912a1b, reversing
    changes made to 1310c7898a79028c76c2c9260eacf4eb8e200cd5.

--- fanin-fanout/src/main/java/edu/asu/ser516/metrics/DailyBurndownPoint.java
+++
@@ -1,9 +0,0 @@
-package edu.asu.ser516.metrics;
-
-import java.time.LocalDate;
-
-public record DailyBurndownPoint(
-        LocalDate date,
-        double remainingWork,
-        double remainingValue) {
-}
