d0cb9ecc52041b6ad78db33cd4ecb6f144bda9d0
air-wickk <ericworwa1118@gmail.com>
2026-04-27 23:17:36 -0700

    Revert changes to StatModel

--- defects-discovered/mongo/models/StatModel.js
+++ defects-discovered/mongo/models/StatModel.js
@@ -1,24 +1,8 @@
 import mongoose from "mongoose";
 
 const StatSchema = new mongoose.Schema({
-  sprintName: {
-    type: String,
-    required: false
-  },
-  sprintStartDate: {
-    type: Date,
-    required: false
-  },
-  sprintEndDate: {
-    type: Date,
-    required: false
-  },
   workCapacity : Number,
-  velocity: {
-    type: Number,
-    required: false,
-    description: "Sum of original estimates of all accepted work items"
-  },
+  velocity : Number,
   scannedAt: {
     type: Date,
     default: Date.now
