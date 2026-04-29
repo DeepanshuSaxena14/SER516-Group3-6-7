711c8bcce09fa8652869457d56a0d26160e3bd53
Sander95 <48042512+sander95ASU@users.noreply.github.com>
2026-04-29 01:41:00 -0700

    Revert "Task 82: Add API/Function to get sprint velocities and store in Supabase"

--- defects-discovered/mongo/routes/VelocityRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getVelocities } from "../controllers/VelocityController.js";
-
-const router = Router();
-
-router.get("/velocity/:projectId", getVelocities);
-
-export default router;
